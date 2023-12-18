5a59e128988eb68773a20b286245607be3be225e
Apoorv Pandey <apoorvrpandey@gmail.com>
2023-12-18 19:10:37 +0530

    deployment-v3

--- client/src/connection/socket.js
+++ client/src/connection/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io({
   reconnection: true, // Enable reconnection
   reconnectionAttempts: 5, // Maximum number of reconnection attempts
   reconnectionDelay: 1000,
